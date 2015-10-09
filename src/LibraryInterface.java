f88c5a23751c38bb404a867c1765565480af2982
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-10-09 10:39:57 -0400

    Lesson 6

---
+++ src/LibraryInterface.java
@@ -0,0 +1,58 @@
+import hsa.*;
+
+public class LibraryInterface {
+
+    
+    public static void main(String[] args) {
+        Console c=new Console();
+        Book b1, b2, b3, b4, b5;
+        b1 = new Book("From Russia With Love","Greg Hines");
+        b2 = new Book("Living Smart","Rita Langill");
+        b3 = new Book("Singing in the Rain","Harry Conner");
+        b4 = new Book("Good Housekeeping","Pat Burns");
+        b5 = new Book("To Be A Model","Lisa Lahey");
+        c.println("Here are the books available:\n");
+        c.println("Book 1:");
+        c.println(""+b1);
+        c.println("Book 2:");
+        c.println(""+b2);
+        c.println("Book 3:");
+        c.println(""+b3);
+        c.println("Book 4:");
+        c.println(""+b4);
+        c.println("Book 5:");
+        c.println(""+b5);
+        Patron p=new Patron("James Gilmor");
+        c.println("Lending 4 books to "+p.getName());
+        if (p.borrow(b1)){
+            c.println(b1.getTitle() +" successfully borrowed.");
+        }
+        else{
+            c.println(b1.getTitle() +" could not be borrowed.");
+        }
+        //next book
+        if (p.borrow(b2)){
+            c.println(b2.getTitle() +" successfully borrowed.");
+        }
+        else{
+            c.println(b2.getTitle() +" could not be borrowed.");
+        }
+        //next book
+        if (p.borrow(b3)){
+            c.println(b3.getTitle() +" successfully borrowed.");
+        }
+        else{
+            c.println(b3.getTitle() +" could not be borrowed.");
+        }
+        //next book
+        if (p.borrow(b4)){
+            c.println(b4.getTitle() +" successfully borrowed.");
+        }
+        else{
+            c.println(b4.getTitle() +" could not be borrowed.");
+        }
+        //return books
+        
+    }
+    
+}
